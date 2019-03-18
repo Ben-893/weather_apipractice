c711084d2b6bf67fe44d4016aa5da8136db0abf1
Benjamin <ben.smith.gordon@gmail.com>
2019-03-18 16:03:33 +0000

    commit fix

---
+++ README.md
@@ -0,0 +1,1 @@
+# A practice app for using of APIs.
--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => console.log("App listening on port 3000!"));
-app.get('/', (req, res) => { res.send("Hi");
+
+app.get('/', (req, res) => { res.render("home", { name: "Ben" });
 });
+
+app.set("view engine", "ejs");
