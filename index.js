0cd1bb5f128c2b05b9fd794d619037406ab83546
Benjamin <ben.smith.gordon@gmail.com>
2019-03-21 14:07:27 +0000

    Made a call to the weather api and displayed the timezone and current summary

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const dotenv = require('dotenv'); dotenv.config();
 
 app.listen(3000, () => console.log("App listening on port 3000!"));
 
-app.get('/', (req, res) => { res.render("home", { name: "Ben" });
+app.get('/', (req, res) => { axios(`https://api.darksky.net/forecast/${process.env.API_KEY}/51.5757,0.1982`, { method: 'GET', }).then((response) => {
+console.log(response); 
+
+res.render("home", { data: response.data }); }).catch((error) => {
+console.log('there was an error', error); });
 });
 
 app.set("view engine", "ejs");
